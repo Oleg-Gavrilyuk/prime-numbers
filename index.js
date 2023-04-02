ee7460314ca6be286822752e41700fea2906ba51
Oleg-Gavrilyuk <gavrilyuk161090@yandex.ru>
2023-04-02 10:04:12 +0300

    v2

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-lst = []
-
 function search(point) {
+    var lst = []
     for (let i = 2;; i++) {
         let flag=1;
         for (let j = 2; (j <= i/2)&&(flag==1); j=j+1) {
@@ -8,10 +7,10 @@ function search(point) {
                 flag=0}
         }       
         if (flag==1) {
-            console.log(i);
             lst.push(i);
         }
         if (lst.length == point) {break}
     }
+    return lst
 }
 console.log(search(process.argv[2]));
